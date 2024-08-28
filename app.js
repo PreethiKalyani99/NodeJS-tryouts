453b98954f2631aee588f588d388366a8f6005f9
PreethiKalyani99 <preethikalyani99@gmail.com>
2024-08-28 22:33:24 +0530

    return a file - codecrafters

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const fs = require("fs")
 
 const server = net.createServer((socket) => {
     socket.on("data", (request) => {
@@ -17,6 +18,16 @@ const server = net.createServer((socket) => {
             const userAgent = headers.find(header => header.toLowerCase().startsWith('user-agent'))
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`)
         }
+        else if (url.toLowerCase().startsWith('/files/')) {
+            const filePath = `.${url}.js`
+            if (fs.existsSync(`${filePath}`)) {
+                const content = fs.readFileSync(`${filePath}`).toString()
+                const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n`
+                socket.write(res);
+              } else {
+                socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+              }
+        } 
         else{
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
         }
