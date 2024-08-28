0949fec6276511b51dd660b012a3b4208f44fdf1
PreethiKalyani99 <preethikalyani99@gmail.com>
2024-08-28 23:10:08 +0530

    Read request body - codecrafters

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const fs = require("fs")
 const server = net.createServer((socket) => {
     socket.on("data", (request) => {
 
-        const url = request.toString().split(" ")[1]
+        const req = request.toString().split(" ")
+        const url = req[1]
         const headers = request.toString().split('\r\n')
+        const method = req[0]
 
+        console.log(request.toString().split(" "), "requestt")
+        console.log(request.toString().split('\r\n'), "hearders")
         if(url === '/'){
             socket.write("HTTP/1.1 200 OK\r\n\r\n")
         }
@@ -18,20 +22,33 @@ const server = net.createServer((socket) => {
             const userAgent = headers.find(header => header.toLowerCase().startsWith('user-agent'))
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`)
         }
-        else if (url.toLowerCase().startsWith('/files/')) {
+        else if (method === 'GET' && url.toLowerCase().startsWith('/files/')) {
             const filePath = `.${url}.js`
             if (fs.existsSync(`${filePath}`)) {
                 const content = fs.readFileSync(`${filePath}`).toString()
                 const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n`
-                socket.write(res);
-              } else {
-                socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-              }
-        } 
-        else{
+                socket.write(res)
+            } else {
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+            }
+        } 
+        else if(method === 'POST' && url.toLowerCase().startsWith('/files/')){
+            const content = request.toString("utf-8").split("\r\n\r\n")[1]
+
+            fs.writeFile(`.${url}`, content, (err) => {
+                if (err) {
+                    console.error('Error writing file:', err);
+                    socket.write("HTTP/1.1 500 INTERNAL SERVER ERROR\r\n\r\n")
+                    return
+                } else {
+                    socket.write("HTTP/1.1 201 CREATED\r\n\r\n")
+                }
+            })
         }
-        socket.end();
+        else{
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+        }
+        socket.end()
     });
 });
 
