56dbb4ba9e20bd50809dfc2eb6948cc7361c3761
PreethiKalyani99 <preethikalyani99@gmail.com>
2024-08-28 22:14:52 +0530

    send response with body - codecrafters

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// initial commit  
+const net = require("net");
+
+const server = net.createServer((socket) => {
+    socket.on("data", (data) => {
+        const request = data.toString();
+        // console.log(request, "request")
+        if (request.includes('/echo/abc ')) {
+          const httpResponse = 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 6\r\n\r\nabcdja';
+          socket.write(httpResponse);
+        } 
+        else {
+            const httpResponse = 'HTTP/1.1 404 Not Found\r\n\r\n';
+            socket.write(httpResponse);
+        }
+        socket.end();
+  });
+});
+
+server.listen(4221);
+console.log("Listening...")
